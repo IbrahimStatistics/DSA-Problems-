76f7553acec0b5563ed5c21cadb5c90f7f2951d2
IbrahimStatistics <IbrahimAhmad21006@gmail.com>
2026-07-05 17:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (76.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.length() != t.length()) return false;
 
-        return s == t;
+        int charc[26];
+
+        for(int i = 0; i<s.size(); i++) {
+            charc[s[i] - 'a']++;
+            charc[t[i] - 'a']--;
+        }
+
+        for(int i : charc)
+            if(i != 0) return false;
+            
+        return true;
     }
 };
