e04aeefdaa177a5944171ec12677f922caec041f
Ibrahim Ahmad <IbrahimAhmad21006@gmail.com>
2026-08-04 19:43:58 +0530

    Time: 314 ms (6.02%) | Memory: 81.5 MB (6.21%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> lastIndex;
-
         int left = 0;
-        int maxLength = 0;
+        int ans = 0;
+        
+        unordered_set<char> charSet;
 
-        for (int right = 0; right < s.size(); right++) {
-
-            // If character already exists in the current window,
-            // move the left pointer
-            if (lastIndex.find(s[right]) != lastIndex.end() &&
-                lastIndex[s[right]] >= left) {
-                left = lastIndex[s[right]] + 1;
+        for(int right = 0; right < s.length(); right++) {
+            while(charSet.find(s[right]) !=charSet.end()) {
+                charSet.erase(s[left]);
+                left++;
             }
 
-            // Update the last seen index of the current character
-            lastIndex[s[right]] = right;
-
-            // Update the maximum window size
-            maxLength = max(maxLength, right - left + 1);
+            charSet.insert(s[right]);
+            ans = max(ans, right - left + 1);
         }
 
-        return maxLength;
+        return ans;
     }
 };
